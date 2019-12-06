68c0c24c2f43fa91ea7134f48ed378ce0844dc82
daocreate <daocreate@gmail.com>
2019-12-06 16:04:09 +0100

    create projet with spatie Role and permission.

---
+++ app/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable implements HasMedia
+{
+    use Notifiable;
+    use HasRoles;
+    use HasMediaTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
