4fae587f1abf2ae62857b862a824d110bc79a44a
daocreate <daocreate@gmail.com>
2019-12-08 05:56:37 +0100

    remove unuse route on web roue file.

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,3 @@ Route::get('/admin1', function () {
 });
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => ['auth']], function() {
-    Route::resource('roles','RoleController');
-    Route::resource('users','UserController');
-    Route::resource('products','ProductController');
-});
