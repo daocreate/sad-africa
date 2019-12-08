5b0a6a911314c6d4bc3aed72ab6c7bdbfcc26a3b
daocreate <daocreate@gmail.com>
2019-12-08 19:21:00 +0100

    formation backend.

--- resources/views/backend/users/show.blade.php
+++ resources/views/backend/users/show.blade.php
@@ -1,6 +1,6 @@
-@extends('BackEnd.layouts.base')
+@extends('backend.layout')
 
-@section('content')
+@section('main')
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
